7f6c93f65326d93bdd67eb88eebd27ac53e23960
scardignoa <antonio.scardigno@post.ch>
2025-03-16 15:01:17 +0100

    Set JVM_17 as target to kotlin and modules

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
     androidTarget {
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
@@ -120,8 +120,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
